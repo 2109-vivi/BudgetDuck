26a56675ade8bd95730e84398758b4cccedcdb4f
bsullit <brian.sullivan.ic@gmail.com>
2021-12-01 15:38:46 -0500

    additional formatting

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -10,14 +10,22 @@ export const Dashboard = (props) => {
   const { username } = props;
 
   return (
-    <div className="dash-container">
-      <div className="left-container">
-        <h4>Transactions List</h4>
+    <div className={"dash-container"}>
 
+      <div className={"left-container"}>
+        <h4>Transactions List</h4>
+        <div className={"listed-transaction"}>listed transaction
+        <button>singleTransaction</button>
+        </div>
+        <div className={"listed-transaction"}>listed transaction
+        <button>singleTransaction</button></div>
+        <div className={"listed-transaction"}>listed transaction
+        <button>singleTransaction</button></div>
       </div>
-      <div className="right-container">
-        <div className="half-containers">Top right</div>
-        <div className="half-containers">Bottom right</div>
+
+      <div className={"right-container"}>
+        <div className={"half-containers"}>Top right graph</div>
+        <div className={"half-containers"}>Bottom right graph</div>
       </div>
     </div>
   );
