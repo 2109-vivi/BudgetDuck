cec7e3b7739dbc514049c44ce04d867c67e48c7b
bsullit <brian.sullivan.ic@gmail.com>
2021-12-02 11:03:24 -0500

    rendering plaid data and adding BudgetChart

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,92 +1,37 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { connect } from "react-redux";
-import { PieChart, Pie, Sector, Cell, Tooltip } from "recharts";
-
+import BudgetChart from "./BudgetChart";
 //API CALLS TO /api/transactions
 
 /**
  * COMPONENT
  */
 
-const dummyTransactions = [
-  {
-    "name": "TOYS R US",
-    "amount": 500,
-    "date": "2021-12-01",
-    "userId": "1",
-    "categoryId": "1"
-},
-{
-  "name": "DINER",
-  "amount": 500,
-  "date": "2021-12-01",
-  "userId": "1",
-  "categoryId": "1"
-},  {
-  "name": "THE SHED",
-  "amount": 70,
-  "date": "2021-12-01",
-  "userId": "1",
-  "categoryId": "1"
-},  {
-  "name": "SUNGLASS DEPOT",
-  "amount": 600,
-  "date": "2021-12-01",
-  "userId": "1",
-  "categoryId": "1"
-},  {
-  "name": "TOYS R US",
-  "amount": 10,
-  "date": "2021-12-01",
-  "userId": "1",
-  "categoryId": "1"
-},
-
-]
 export const Dashboard = (props) => {
   const { username } = props;
+
+  const transactions = useSelector((state) => state.transactions);
 
   return (
     <div className={"dash-container"}>
       <div className={"left-container"}>
-        <h4>
+        <h4 className={"list-header"}>
           Todays Transactions
           <button>+</button>
+          <button>...</button>
         </h4>
-        <div className={"listed-transaction"}>
-          listed transaction
-          <button>singleTransaction</button>
-        </div>
-        <div className={"listed-transaction"}>
-          listed transaction
-          <button>singleTransaction</button>
-        </div>
-        <div className={"listed-transaction"}>
-          listed transaction
-          <button>singleTransaction</button>
-        </div>
-        <div className={"listed-transaction"}>
-          listed transaction
-          <button>singleTransaction</button>
-        </div>
-        <div className={"listed-transaction"}>
-          listed transaction
-          <button>singleTransaction</button>
-        </div>
-      </div>
-
-      <div className={"right-container"}>
-        <div className={"half-containers"}>
-          Categorical Spending Component
-          <div>
-            <PieChart width={300} height={300}>
-              <Pie
-              fill='#8884d8'/>
-              <Tooltip />
-            </PieChart>
+      {transactions.map((transaction) => {
+        return (
+          <div key={transaction.id}className={"listed-transaction"}>
+            <p>{transaction.name} ... ${transaction.amount}</p>
           </div>
-        </div>
-        <div className={"half-containers"}>Budget Component</div>
+        );
+      })}
+    </div>
+      <div className={"right-container"}>
+        <div className={"half-containers"}>Categorical Spending Component</div>
+        <div className={"half-containers"}>{BudgetChart()}</div>
       </div>
     </div>
   );
