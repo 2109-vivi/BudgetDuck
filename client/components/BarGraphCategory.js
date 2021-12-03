4e01955611ae2067cc33834fcb878f813eea4b44
Victorywastaken <rlatmdfl92@gmail.com>
2021-12-03 16:56:02 -0500

    Added individual colors to bargraph-component

--- client/components/BarGraphCategory.js
+++ client/components/BarGraphCategory.js
@@ -1,70 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { getTransactionsFromDatabase } from '../store/transactions';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 const BarGraphCategory = (props) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getTransactionsFromDatabase(props.accessToken));
-  }, []);
 
   let transactions = useSelector((state) => state.transactions || []);
 
+  const barColors = ["#D99694", "#FAC090", '#FFFD70', "#C3D69B", "#93CDDD", "#558ED5", ]
   const dataArray = transactions.map((transaction) => {
     return {
       category: transaction.category.categoryName,
       amount: transaction.amount,
     };
   });
-
-  console.log('dataArray', dataArray);
 
   let output = [];
 
@@ -82,7 +30,8 @@ const BarGraphCategory = (props) => {
 
   console.log(output);
   output.forEach(category => {
-    category.budget = 5000;
+    //random number from 2000 to 5000
+    category.budget = Math.floor(Math.random() * (5000 - 2000 + 1)) + 2000;
   })
 
   return (
@@ -103,8 +52,16 @@ const BarGraphCategory = (props) => {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="amount" stackId="a" fill="#3DD7B2" />
-          <Bar dataKey="budget" stackId="b" fill="#82ca9d" />
+          <Bar dataKey="amount" stackId="a" fill="#8884d8">
+            {
+              output.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={barColors[index % 20]}/>
+            ))
+            }
+          </Bar>
+          <Bar dataKey="budget" stackId="b" fill="#3DD7B2">
+          </Bar>
+
         </BarChart>
       </ResponsiveContainer>
   );
--- script/userTransactions.js
+++ script/userTransactions.js
@@ -1,56 +1,301 @@
-module.exports = [
-  {
-    bankTransactionId: null,
-    name: 'Combe Incorporated',
-    merchantName: null,
-    amount: 136.75,
-    date: '2021-08-11',
-    userId: 2,
-    categoryId: 1,
-  },
-  {
-    bankTransactionId: null,
-    name: 'Home Sweet Homeopathics',
-    merchantName: null,
-    amount: 836.93,
-    date: '2021-05-03',
-    userId: 2,
-    categoryId: 4,
-  },
-  {
-    bankTransactionId: null,
-    name: 'American Sales Company',
-    merchantName: null,
-    amount: 989.2,
-    date: '2021-10-12',
-    userId: 2,
-    categoryId: 2,
-  },
-  {
-    bankTransactionId: null,
-    name: 'Cypress Pharmaceutical, Inc.',
-    merchantName: null,
-    amount: 399.84,
-    date: '2020-12-31',
-    userId: 1,
-    categoryId: 2,
-  },
-  {
-    bankTransactionId: null,
-    name: 'Personal Care Products',
-    merchantName: null,
-    amount: 526.42,
-    date: '2021-01-30',
-    userId: 1,
-    categoryId: 3,
-  },
-  {
-    bankTransactionId: null,
-    name: 'Bryant Ranch Prepack',
-    merchantName: null,
-    amount: 469.84,
-    date: '2021-05-26',
-    userId: 1,
-    categoryId: 2,
-  },
-];
+module.exports = [{
+  "bankTransactionId": null,
+  "name": "HyVee Inc",
+  "merchantName": null,
+  "amount": 793.64,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "Heel Inc",
+  "merchantName": null,
+  "amount": 880.94,
+  "date": "2021-11-19"
+}, {
+  "bankTransactionId": null,
+  "name": "Lake Erie Medical DBA Quality Care Products LLC",
+  "merchantName": null,
+  "amount": 798.1,
+  "date": "2021-11-09"
+}, {
+  "bankTransactionId": null,
+  "name": "SPIRIT PHARMACEUTICALS,LLC",
+  "merchantName": null,
+  "amount": 194.37,
+  "date": "2021-12-02"
+}, {
+  "bankTransactionId": null,
+  "name": "Omega Tech Labs Inc.",
+  "merchantName": null,
+  "amount": 930.24,
+  "date": "2021-11-22"
+}, {
+  "bankTransactionId": null,
+  "name": "Air Liquide Healthcare America Corporation",
+  "merchantName": null,
+  "amount": 792.95,
+  "date": "2021-11-19"
+}, {
+  "bankTransactionId": null,
+  "name": "Target Corporation",
+  "merchantName": null,
+  "amount": 956.99,
+  "date": "2021-11-18"
+}, {
+  "bankTransactionId": null,
+  "name": "Unit Dose Services",
+  "merchantName": null,
+  "amount": 984.79,
+  "date": "2021-11-25"
+}, {
+  "bankTransactionId": null,
+  "name": "A-S Medication Solutions LLC",
+  "merchantName": null,
+  "amount": 833.48,
+  "date": "2021-11-14"
+}, {
+  "bankTransactionId": null,
+  "name": "Aidance Skincare & Topical Solutions, LLC",
+  "merchantName": null,
+  "amount": 743.61,
+  "date": "2021-11-23"
+}, {
+  "bankTransactionId": null,
+  "name": "Bausch & Lomb Incorporated",
+  "merchantName": null,
+  "amount": 501.57,
+  "date": "2021-11-17"
+}, {
+  "bankTransactionId": null,
+  "name": "REMEDYREPACK INC.",
+  "merchantName": null,
+  "amount": 261.39,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "Uriel Pharmacy Inc.",
+  "merchantName": null,
+  "amount": 998.35,
+  "date": "2021-11-11"
+}, {
+  "bankTransactionId": null,
+  "name": "REMEDYREPACK INC.",
+  "merchantName": null,
+  "amount": 192.1,
+  "date": "2021-11-29"
+}, {
+  "bankTransactionId": null,
+  "name": "Boehringer Ingelheim Pharmaceuticals Inc.",
+  "merchantName": null,
+  "amount": 432.06,
+  "date": "2021-11-18"
+}, {
+  "bankTransactionId": null,
+  "name": "Carilion Materials Management",
+  "merchantName": null,
+  "amount": 199.24,
+  "date": "2021-11-17"
+}, {
+  "bankTransactionId": null,
+  "name": "Preferred Pharmaceuticals, Inc.",
+  "merchantName": null,
+  "amount": 299.7,
+  "date": "2021-11-23"
+}, {
+  "bankTransactionId": null,
+  "name": "TAI GUK PHARM. CO., LTD.",
+  "merchantName": null,
+  "amount": 533.78,
+  "date": "2021-11-15"
+}, {
+  "bankTransactionId": null,
+  "name": "Aton Pharma, Inc.",
+  "merchantName": null,
+  "amount": 127.67,
+  "date": "2021-11-21"
+}, {
+  "bankTransactionId": null,
+  "name": "Greenstone LLC",
+  "merchantName": null,
+  "amount": 351.25,
+  "date": "2021-11-30"
+}, {
+  "bankTransactionId": null,
+  "name": "Uriel Pharmacy Inc.",
+  "merchantName": null,
+  "amount": 93.58,
+  "date": "2021-11-13"
+}, {
+  "bankTransactionId": null,
+  "name": "Go Smile, Inc.",
+  "merchantName": null,
+  "amount": 524.35,
+  "date": "2021-11-22"
+}, {
+  "bankTransactionId": null,
+  "name": "Allergy Laboratories, Inc.",
+  "merchantName": null,
+  "amount": 325.73,
+  "date": "2021-11-17"
+}, {
+  "bankTransactionId": null,
+  "name": "CVS Pharmacy",
+  "merchantName": null,
+  "amount": 386.41,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "DZA Brands LLC",
+  "merchantName": null,
+  "amount": 63.98,
+  "date": "2021-11-25"
+}, {
+  "bankTransactionId": null,
+  "name": "Apotex Corp.",
+  "merchantName": null,
+  "amount": 479.06,
+  "date": "2021-11-29"
+}, {
+  "bankTransactionId": null,
+  "name": "White's Inc.",
+  "merchantName": null,
+  "amount": 914.91,
+  "date": "2021-11-08"
+}, {
+  "bankTransactionId": null,
+  "name": "Novartis Pharmaceuticals Corporation",
+  "merchantName": null,
+  "amount": 895.03,
+  "date": "2021-11-21"
+}, {
+  "bankTransactionId": null,
+  "name": "Aphena Pharma Solutions - Tennessee, Inc.",
+  "merchantName": null,
+  "amount": 545.44,
+  "date": "2021-11-27"
+}, {
+  "bankTransactionId": null,
+  "name": "REMEDYREPACK INC.",
+  "merchantName": null,
+  "amount": 693.0,
+  "date": "2021-11-16"
+}, {
+  "bankTransactionId": null,
+  "name": "Pharmacia and Upjohn Company",
+  "merchantName": null,
+  "amount": 722.48,
+  "date": "2021-11-11"
+}, {
+  "bankTransactionId": null,
+  "name": "State of Florida DOH Central Pharmacy",
+  "merchantName": null,
+  "amount": 315.1,
+  "date": "2021-11-30"
+}, {
+  "bankTransactionId": null,
+  "name": "Greenstone LLC",
+  "merchantName": null,
+  "amount": 625.15,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "Golden State Medical Supply, Inc",
+  "merchantName": null,
+  "amount": 847.38,
+  "date": "2021-11-14"
+}, {
+  "bankTransactionId": null,
+  "name": "Nelco Laboratories, Inc.",
+  "merchantName": null,
+  "amount": 693.85,
+  "date": "2021-11-11"
+}, {
+  "bankTransactionId": null,
+  "name": "Antigen Laboratories, Inc.",
+  "merchantName": null,
+  "amount": 79.69,
+  "date": "2021-11-14"
+}, {
+  "bankTransactionId": null,
+  "name": "Kinray",
+  "merchantName": null,
+  "amount": 525.58,
+  "date": "2021-11-20"
+}, {
+  "bankTransactionId": null,
+  "name": "Med-Health Pharma, LLC",
+  "merchantName": null,
+  "amount": 403.96,
+  "date": "2021-11-08"
+}, {
+  "bankTransactionId": null,
+  "name": "Golden State Medical Supply, Inc.",
+  "merchantName": null,
+  "amount": 820.1,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "Par Pharmaceutical, Inc.",
+  "merchantName": null,
+  "amount": 730.23,
+  "date": "2021-11-18"
+}, {
+  "bankTransactionId": null,
+  "name": "State of Florida DOH Central Pharmacy",
+  "merchantName": null,
+  "amount": 711.98,
+  "date": "2021-11-30"
+}, {
+  "bankTransactionId": null,
+  "name": "Eli Lilly and Company",
+  "merchantName": null,
+  "amount": 156.71,
+  "date": "2021-11-22"
+}, {
+  "bankTransactionId": null,
+  "name": "Profesional Disposables International, Inc.",
+  "merchantName": null,
+  "amount": 498.16,
+  "date": "2021-11-12"
+}, {
+  "bankTransactionId": null,
+  "name": "Nelco Laboratories, Inc.",
+  "merchantName": null,
+  "amount": 620.83,
+  "date": "2021-12-01"
+}, {
+  "bankTransactionId": null,
+  "name": "Conopco Inc. d/b/a Unilever",
+  "merchantName": null,
+  "amount": 52.44,
+  "date": "2021-11-23"
+}, {
+  "bankTransactionId": null,
+  "name": "Lofthouse of Fleetwood, Ltd.",
+  "merchantName": null,
+  "amount": 147.39,
+  "date": "2021-11-27"
+}, {
+  "bankTransactionId": null,
+  "name": "Rebel Distributors Corp.",
+  "merchantName": null,
+  "amount": 919.24,
+  "date": "2021-11-16"
+}, {
+  "bankTransactionId": null,
+  "name": "Pace Wellness Inc.",
+  "merchantName": null,
+  "amount": 112.55,
+  "date": "2021-11-24"
+}, {
+  "bankTransactionId": null,
+  "name": "CVS Pharmacy",
+  "merchantName": null,
+  "amount": 342.34,
+  "date": "2021-11-08"
+}, {
+  "bankTransactionId": null,
+  "name": "Bryant Ranch Prepack",
+  "merchantName": null,
+  "amount": 864.34,
+  "date": "2021-11-08"
+}]
