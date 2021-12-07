1a6e989d647ef5fae18d920ed028bc91939d724e
Chris E <14ceng@gmail.com>
2021-12-06 22:55:43 -0500

    Chart colors filled and it reacts accordingly to any budget changes. I think I'm going crazy

--- client/components/ChartComponents/StackedBudgetChart.js
+++ client/components/ChartComponents/StackedBudgetChart.js
@@ -1,55 +1,68 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import barColors from './assets/categoryColors';
 import { currentMonth, monthChart } from './assets/constants';
 
 
 const StackedBudgetChart = () => {
   const budgetCategories = useSelector((state) => state.auth.categoricalBudgets || [])
+  const monthlyBudget = useSelector((state) => state.auth.monthlyBudget)
 
+  //Our data object we'll be using for the bar chart is born.
   let dataObj =[{}]
 
+  //Filling our dataObj with budget names and amounts....
   budgetCategories.filter((item)=> {
-    if(item.budgetCategories.length > 0 && item.budgetCategories[0] !== 0){
+    if(item.budgetCategories.length > 0 && item.budgetCategories[0].budgetForCategory !== 0){
       return item
     }
   }).map((element) => {
     dataObj[0][[element.categoryName]] = element.budgetCategories[0].budgetForCategory
   })
+
   //Totals the values of our object to give us our total budget amount
   const totalBudgetCategoryAmount = Object.values(dataObj[0]).reduce((acc, total) => {
     return acc + total
   },0)
 
   //Adds an unallocated key to our datobject with totalBudgetCategoryAmount
-  dataObj[0]["Unallocated"] = totalBudgetCategoryAmount
+  dataObj[0]["Unallocated"] = monthlyBudget - totalBudgetCategoryAmount
   //Adds a name key with the current month
   dataObj[0]["name"]= `Budget for ${monthChart[currentMonth]}`
 
-  const data = [
-    {
-      name: `Budget for ${monthChart[currentMonth]}`,
-      Food: 4000,
-      Bank: 2400,
-      Community: 5000,
-      Unallocated: 3000,
-    }
-  ];
+
+  //Colors will always be in ROYGBIV  ascending order, but the last index of the array will ALWAYS be grey (Unallocated funds). Just FYI I use Object.keys(dataObj[0]).length-2 (instead of 1) here because the last key is name which won't be used to calculate how many colors we're using.
+  const colorsArray = barColors.slice(0,Object.keys(dataObj[0]).length-2)
+  colorsArray.push('#808080')
+
+  //This is to set colors for each budget category so they stack accordingly. I set an empty array and add each Bar component to it (this is so recharts can read it). From there, we have our colors array already imported as barColors. I set up an counter to track the index, and then incremented each iteration so we get a different color each time. Please no more colors my brain hurts
+
+  let index =0
+  let bars = []
+
+  for(let key in dataObj[0]) {
+    bars.push( <Bar key={key} dataKey={key} stackId="a" fill={colorsArray[index % colorsArray.length]} />)
+    index++
+  }
+  //As seen on line 28, I added a name key for the sake of adding a title to the bar chart. However, since I don't want the name key to show up in the tooltip (or the stack for that matter) I get rid of it here since it will ALWAYS be the last index of the array.
+  bars.pop()
+
   return (
     <div>
-      {console.log(dataObj)}
+      {console.log(budgetCategories)}
+      {/* {console.log(colorsArray)} */}
       <h1 style={{textAlign : 'center'}}>
     Stacked Budget Chart
       </h1>
       <ResponsiveContainer width="100%" height={500}>
-        <BarChart data={data} >
+        <BarChart data={dataObj} >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
+          <YAxis domain={[0, dataMax => Math.round((dataMax + 1000)/1000)* 1000 ]}/>
+          <Tooltip formatter={(value) => `$${value}`}/>
           <Legend />
-          <Bar dataKey="Food" stackId="a" fill="#8884d8" />
-          <Bar dataKey="Unallocated"stackId="a" fill="#82ca9d" />
+          {bars}
         </BarChart>
       </ResponsiveContainer>
     </div>
